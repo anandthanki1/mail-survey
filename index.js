db8ece763ce5c296f0a9cf3e7bc584c7d8d1788a
Anand Thanki <anandthanki@Anands-MBP.home>
2020-05-03 15:32:01 -0700

    Worked on setting up mongodb and mongoose

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
+// mongoose recommends adding useNewUrlParser and useUnifiedTopology
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
 const app = express();
 
 // Below line figures out PORT number from underlying environment
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('My App is Working. Hurray!!!');
-});
+authRoutes(app);
 
 app.listen(PORT, () => {
     console.log(`Server running on : ${PORT}`);

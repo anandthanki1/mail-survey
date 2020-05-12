df93584bc7abda7d87b1676316fc1a39a3475403
anandthanki1 <anandthanki18@gmail.com>
2020-05-11 19:35:20 -0700

    Added serialize and deserialize user functions and also added logout functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 /* While using below require please be cautious about order
 that you want to execute because some operations will be dependent on modules
@@ -12,6 +14,16 @@ const authRoutes = require('./routes/authRoutes');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 100,
+        keys: [keys.cookieKey],
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Below line figures out PORT number from underlying environment
 const PORT = process.env.PORT || 5000;

0eff2f9a34cb552ed02c53fc9894852ac4517816
anandthanki1 <anandthanki18@gmail.com>
2020-05-21 23:43:39 -0700

    Updated Google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -28,7 +28,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true,
 }, (accessToken, refreshToken, profile, done) => {
     /* findOne finds the one document we requested */
     User.findOne({ googleId: profile.id })
